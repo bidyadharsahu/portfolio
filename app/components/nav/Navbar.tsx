94802980829dde1e16564a5c1dedf4d1c55f93d4
bidyadharsahu <131295002+bidyadharsahu@users.noreply.github.com>
2024-11-20 21:32:14 +0530

    Update Navbar.tsx

--- app/components/nav/Navbar.tsx
+++ app/components/nav/Navbar.tsx
@@ -4,7 +4,7 @@ import MobileNavDropdown from './MobileNavDropdown'
 
 const Logo = ({ className }: { className?: string }) => {
   return (
-    <img alt="logo" src="/home.png" className={`w-10 ${className}`} />
+    <img alt="logo" src="/home.png" className={`w-9 ${className}`} />
   )
 }
 
